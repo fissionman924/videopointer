19973ee62a4c7d6a0c377112f04da8ca9be65e44
fissionman924 <nathaniel.lord@gmail.com>
2016-05-24 09:29:10 -0600

    Variable Names, Midpoint, Event Listener Updates

--- demo/video-pointer.js
+++ demo/video-pointer.js
@@ -58,7 +58,7 @@
             context.generatePoints.apply(context);
         });
         $(context.options.videoSelector, this.$element).on("canplay", function(event) {
-            context.rerender.apply(context);
+            context.generatePoints.apply(context);
         });
     };
 
@@ -157,11 +157,11 @@
             }
 
             //Define points (point on video: pointA, point at expander icon: pointB, and midPoint)
-            var pointA = {
+            var endPoint = {
                 x: 0,
                 y: 0
             };
-            var pointB = {
+            var anchorPoint = {
                 x: 0,
                 y: 0
             };
@@ -171,13 +171,13 @@
             };
 
             //Get the location on the video we're interested in
-            pointA.x = (currentPoint.x / 100) * vid.w + vid.x;
-            pointA.y = (currentPoint.y / 100) * vid.h + vid.y;
+            endPoint.x = (currentPoint.x / 100) * vid.w + vid.x;
+            endPoint.y = (currentPoint.y / 100) * vid.h + vid.y;
 
             //Find the absolute position of the expander icon on the page
             var $expander = $(element);
-            pointB.x = $expander.position().left;
-            pointB.y = $expander.position().top + $expander.outerHeight() / 2;
+            anchorPoint.x = $expander.position().left;
+            anchorPoint.y = $expander.position().top + $expander.outerHeight() / 2;
 
             //Solve for the sides of the imaginary triangle so we can angle the pointer
             var angles = {
@@ -186,53 +186,53 @@
                 C: 180 - angle - 90
             };
             var sides = {
-                a: Math.abs(pointA.y - pointB.y),
+                a: Math.abs(endPoint.y - anchorPoint.y),
                 b: 0,
                 c: 0
             };
             sides.b = sides.a * Math.sin(Math.radians(angles.B)) / Math.sin(Math.radians(angles.A));
             sides.c = sides.a * Math.sin(Math.radians(angles.C)) / Math.sin(Math.radians(angles.A));
 
-            if (pointA.x > pointB.x) {
-                midPoint.x = pointB.x + sides.c;
+            if (endPoint.x > anchorPoint.x) {
+                midPoint.x = endPoint.x - sides.c;
             } else {
-                midPoint.x = pointA.x + sides.c;
+                midPoint.x = endPoint.x + sides.c;
             }
             //Determine if we can stick with the predefined angle or if we need to switch to a 90 degree angle based on proximity
-            if (Math.abs(pointA.x - pointB.x) < Math.abs(pointA.y - pointB.y)) {
-                midPoint.x = pointA.x;
+            if (Math.abs(endPoint.x - anchorPoint.x) < Math.abs(endPoint.y - anchorPoint.y)) {
+                midPoint.x = endPoint.x;
             }
 
-            midPoint.y = pointB.y;
+            midPoint.y = anchorPoint.y;
 
             //Create the image and add it to this pointer item
             image = null;
-            image = drawAngle.call(context, pointA, midPoint, pointB, currentPoint.color);
+            image = drawAngle.call(context, endPoint, midPoint, anchorPoint, currentPoint.color);
             $(element).parents(context.options.itemSelector).append(image);
             $(image).css("position", "absolute");
 
             //Place the image at the correct location based on whether the angle is going down or up
             var vOffset = 1;
             if ($(image).height() <= 12) {
-                vOffset = (context.options.endPointRadius + context.options.endPointStroke) - Math.abs(pointB.y - pointA.y);
+                vOffset = (context.options.endPointRadius + context.options.endPointStroke) - Math.abs(anchorPoint.y - endPoint.y);
             }
 
-            if (pointA.x > pointB.x) {
+            if (endPoint.x > anchorPoint.x) {
                 $(image).css({
-                    "left": pointB.x + "px"
+                    "left": anchorPoint.x + "px"
                 });
             } else {
                 $(image).css({
-                    "left": pointB.x - image.width + "px"
+                    "left": anchorPoint.x - image.width + "px"
                 });
             }
-            if (pointA.y > pointB.y) {
+            if (endPoint.y > anchorPoint.y) {
                 $(image).css({
-                    "top": pointB.y - vOffset + "px"
+                    "top": anchorPoint.y - vOffset + "px"
                 });
             } else {
                 $(image).css({
-                    "top": pointB.y - image.height + vOffset + "px"
+                    "top": anchorPoint.y - image.height + vOffset + "px"
                 });
             }
             $(image).addClass("pointer");
@@ -240,20 +240,24 @@
     };
 
     //Create the images of the angled pointers
-    var drawAngle = function(pointA, pointB, pointC, color) {
+    var drawAngle = function(endPoint, midPoint, anchorPoint, color) {
         var image = new Image();
         var endPointRadius = this.options.endPointRadius;
 
+        if (midPoint.x !== endPoint.x && Math.abs(midPoint.x - endPoint.x) !== Math.abs(midPoint.y - endPoint.y)) {
+            console.log(endPoint, midPoint);
+        }
+
         var points = {
-            a: {},
-            b: {},
-            c: {}
+            anchor: {},
+            mid: {},
+            end: {}
         };
 
         //Get 
         var dims = {
-            w: Math.ceil(Math.abs(pointC.x - pointA.x) + endPointRadius + 6),
-            h: Math.ceil(Math.abs(pointA.y - pointC.y) + endPointRadius + 6)
+            w: Math.ceil(Math.abs(anchorPoint.x - endPoint.x) + endPointRadius + 6),
+            h: Math.ceil(Math.abs(endPoint.y - anchorPoint.y) + endPointRadius + 6)
         };
         //Create the canvas element in memory and define its dimensions
         var canvas = document.createElement("canvas");
@@ -264,36 +268,36 @@
 
         //Add an offset if the vertical difference in points is minimal
         var vOffset = 0;
-        if (Math.abs(pointC.y - pointA.y) <= endPointRadius) {
-            vOffset = (endPointRadius + 2) - Math.abs(pointC.y - pointA.y);
+        if (Math.abs(anchorPoint.y - endPoint.y) <= endPointRadius) {
+            vOffset = (endPointRadius + 2) - Math.abs(anchorPoint.y - endPoint.y);
             if (vOffset < 0) {
                 vOffset = 0;
             }
         }
         //Pointer coming from right or left?
-        if (pointA.x > pointC.x) {
-            points.a.x = 1;
-            points.b.x = pointB.x - pointC.x;
-            points.c.x = pointA.x - pointC.x;
+        if (endPoint.x > anchorPoint.x) {
+            points.anchor.x = 1;
+            points.mid.x = midPoint.x - anchorPoint.x;
+            points.end.x = endPoint.x - anchorPoint.x;
         } else {
-            points.a.x = dims.w;
-            points.b.x = dims.w - (pointC.x - pointB.x);
-            points.c.x = dims.w - (pointC.x - pointA.x);
+            points.anchor.x = dims.w;
+            points.mid.x = dims.w - (anchorPoint.x - midPoint.x);
+            points.end.x = dims.w - (anchorPoint.x - endPoint.x);
         }
         //Pointer coming from top or bottom?
-        if (pointA.y > pointC.y) {
-            points.a.y = 1;
-            points.b.y = 1;
-            points.c.y = pointA.y - pointB.y;
-        } else if (pointC.y > pointA.y) {
-            points.a.y = dims.h - vOffset - 1;
-            points.b.y = dims.h - vOffset - 1;
-            points.c.y = dims.h - (pointB.y - pointA.y) - vOffset - 1;
+        if (endPoint.y > anchorPoint.y) {
+            points.anchor.y = 1;
+            points.mid.y = 1;
+            points.end.y = endPoint.y - midPoint.y;
+        } else if (anchorPoint.y > endPoint.y) {
+            points.anchor.y = dims.h - vOffset - 1;
+            points.mid.y = dims.h - vOffset - 1;
+            points.end.y = dims.h - (midPoint.y - endPoint.y) - vOffset - 1;
         }
 
-        drawLine(drawing, points.a, points.b, 2, this.options.lineColor);
-        drawLine(drawing, points.b, points.c, 2, this.options.lineColor);
-        drawCircle(drawing, points.c, endPointRadius, 2, color, this.options.lineColor);
+        drawLine(drawing, points.anchor, points.mid, 2, this.options.lineColor);
+        drawLine(drawing, points.mid, points.end, 2, this.options.lineColor);
+        drawCircle(drawing, points.end, endPointRadius, 2, color, this.options.lineColor);
 
         image.src = canvas.toDataURL("image/png");
         return image;
